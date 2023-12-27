16b79637ca3abd4816a080ede2c38504719f5824
Shreyas <121279572+Shreyasssonar@users.noreply.github.com>
2023-12-27 05:28:15 +0000

    Your commit message

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+// About.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About This Application</h2>
+      <p>This is a simple React application using React Router.</p>
+    </div>
+  );
+};
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './components/App.js';
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
